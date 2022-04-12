e37064ceb3edcea2e263c67f00b6a8c5ba9bd5b1
CorentinDelalle <corentindelalle@gmail.com>
2022-04-12 15:38:06 -0400

    final

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('#button').addEventListener('click', logMessages); {
+    
+  }
+  
+  function logMessages() {
+    console.log('Message //Versionner ici');
+    console.warn('Attention! //Versionner encore');
+    console.error(`ERREUR //Versionner une autre fois`);
+  }
+  
+  
